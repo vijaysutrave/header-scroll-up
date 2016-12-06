19e367a14efec56eaed7bb5af87b2f4e1eabe539
Vijay Sutrave <vijaysutrave01@gmail.com>
2016-12-07 00:32:30 +0530

    Removed unneccessary files

--- src/index.js
+++ src/index.js
@@ -1,93 +1,96 @@
+/* Default options */
 const options = {
-	defaultCss: true,
-	animationDelay: 0.2,
-	topOffset: 0
+    defaultCss: true,
+    animationDelay: 0.2,
+    topOffset: 0
 };
 
+/* Will add default styles to the header */
 const commonStyles = {
-	'transition': 'all 0.2s ease-in-out',
-	'position': 'fixed',
-	'top': 0, 
-	'left': 0,
-	'width': '100%'
+    'transition': 'all 0.2s ease-in-out',
+    'position': 'fixed',
+    'top': 0,
+    'left': 0,
+    'width': '100%'
 };
 
+/* Reset header position */
 const unStyleObject = {
-	top: '0px'
+    top: '0px'
 }
 
 let setScrollableHeader = (header, userOptions) => {
 
-	if(typeof header === 'string') {
-		header = document.querySelectorAll(header)[0];
-	}
+    if (typeof header === 'string') {
+        header = document.querySelectorAll(header)[0];
+    }
 
-	if(userOptions.animationDelay && typeof userOptions.animationDelay === 'number') {
-		commonStyles['transition'] = 'all '+userOptions.animationDelay+'s ease-in-out'
-	}
+    if (!header) {
+        throw new Error('Header element not found');
+    }
 
-	Object.assign(options, userOptions);
+    if (typeof header === 'object' && !header.nodeName) {
+        throw new Error('Header element not found');
+    }
 
-	if (!header) {
-		throw new Error('Header element not found');
-	}
+    if (options.defaultCss === false && !options.classname) {
+        throw new Error('Class Name to add not provided');
+    }
 
-	if (typeof header === 'object' && !header.nodeName) {
-		throw new Error('Header element not found');		
-	}
+    if (userOptions.animationDelay && typeof userOptions.animationDelay === 'number') {
+        commonStyles['transition'] = 'all ' + userOptions.animationDelay + 's ease-in-out'
+    }
 
-	if (options.defaultCss === false && !options.classname) {
-		throw new Error('Class Name to add not provided');
-	}
+    /* Extend the options */
+    Object.assign(options, userOptions);
 
+    if (options.defaultCss) {
+        Object.assign(header.style, commonStyles);
+    }
 
-	if (options.defaultCss) {
-		Object.assign(header.style, commonStyles);
-	}
+    const headerHeight = header.clientHeight;
 
-	const headerHeight = header.clientHeight;
+    let styleObject = {
+        top: -(headerHeight) + 'px'
+    }
 
-	let styleObject = {
-		top: -(headerHeight)+'px'
-	}
+    let prevScrollTop = 0;
 
-	let prevScrollTop = 0;
+    const callbackMapper = {
+        'true': function(direction) {
+            if (direction === 'down') {
+                Object.assign(header.style, styleObject);
+            } else {
+                Object.assign(header.style, unStyleObject);
+            }
+        },
+        'false': function(direction) {
+            if (direction === 'down') {
+                header.classList.remove(options.classname);
+            } else {
+                header.classList.add(options.classname);
+            }
+        }
+    }
 
-	const callbackMapper = {
-		'true': function(direction) {
-			if(direction === 'down') {
-				Object.assign(header.style, styleObject);
-			} else {
-				Object.assign(header.style, unStyleObject);
-			}
-		}, 	
-		'false': function(direction) {
-			if(direction === 'down') {
-				header.classList.remove(options.classname);
-			} else {
-				header.classList.add(options.classname);
-			}
-		}
-	}
+    /* Main scroll function */
+    let pageScrolled = (e) => {
+        if (pageYOffset < options.topOffset) {
+            return;
+        }
 
+        if (window.pageYOffset > prevScrollTop) {
+            callbackMapper[options.defaultCss]('down')
+        } else {
+            callbackMapper[options.defaultCss]('up')
+        }
+        prevScrollTop = pageYOffset;
+    };
 
-	let pageScrolled = (e) => {
-		if (pageYOffset < options.topOffset) {
-			return;
-		}
-
-		if(window.pageYOffset > prevScrollTop) {	
-			callbackMapper[options.defaultCss]('down')
-	    } else {
-	    	callbackMapper[options.defaultCss]('up')
-	    }
-	    prevScrollTop = pageYOffset;
-	};	
-
-	document.addEventListener('scroll', pageScrolled, false);
+    document.addEventListener('scroll', pageScrolled, false);
 
 }
 
 module.exports = {
-	setScrollableHeader
+    setScrollableHeader
 };
